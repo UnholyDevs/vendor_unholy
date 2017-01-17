006e39241d578a61cd57e78329c2f02d5b125238
usmcamgrimm <usmcamgrimm@gmail.com>
2017-01-17 22:45:13 +0100

    Clean up merge.
    You're welcome buddy. :-)

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:sepolicy/sepolicy.mk
 # Copyright (C) 2017 UnholyDevs
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
