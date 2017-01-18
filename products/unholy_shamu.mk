7cf7dc06b83aff0e6a4ec33ee841799706af9f3e
jl90 <leonardjason90.jl@gmail.com>
2017-01-18 06:08:06 -0700

    Use aosp_shamu.mk

--- products/unholy_shamu.mk
+++ products/unholy_shamu.mk
@@ -30,7 +30,7 @@ $(call inherit-product, vendor/unholy/products/shamu.mk)
 $(call inherit-product, vendor/unholy/configs/unholy_common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/motorola/shamu/full_shamu.mk)
+$(call inherit-product, device/motorola/shamu/aosp_shamu.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := shamu
