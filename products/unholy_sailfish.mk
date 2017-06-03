f8c95bdf9692ee178b51fc71a344dd26af865c47
vic <bamavic1971@gmail.com>
2017-06-03 10:52:37 -0400

    add sailfish mk

---
+++ products/unholy_sailfish.mk
@@ -0,0 +1,38 @@
+#
+# Copyright 2017 Unholy Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common Unholy stuff.
+$(call inherit-product, vendor/unholy/configs/unholy_common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/marlin/aosp_sailfish.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sailfish
+PRODUCT_NAME := unholy_sailfish
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 
+PRODUCT_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sailfish \
+    BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.2/NHG47L/3888830:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 7.1.2 NHG47L 3888830 release-keys"
