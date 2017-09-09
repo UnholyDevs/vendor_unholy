9d3ce1d43617245870bb76b3b6c618c1be94ce1e
Adrian Grimm <usmcamgrimm@gmail.com>
2017-09-09 14:41:10 +0000

    Fix changelog
    Adapted from Nitrogen
    https://github.com/nitrogen-project/android_vendor_nitrogen/blob/o/tools/changelog.sh

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -4,9 +4,6 @@
 dir=$ANDROID_BUILD_TOP
 out=$dir/out/target/product
 
-# Kill jack-server
-sh $dir/prebuilts/sdk/tools/jack-admin kill-server
-
 export Changelog=Changelog.txt
 
 if [ -f $Changelog ];
@@ -19,20 +16,21 @@ touch $Changelog
 # Print something to build output
 echo ${bldppl}"Generating changelog..."${txtrst}
 
-for i in $(seq 14);
+for i in $(seq 10);
 do
 export After_Date=`date --date="$i days ago" +%m-%d-%Y`
 k=$(expr $i - 1)
 	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
 
 	# Line with after --- until was too long for a small ListView
-	echo '===============' >> $Changelog;
-	echo  "  "$Until_Date    >> $Changelog;
-	echo '===============' >> $Changelog;
-	# Cycle through every repo to find commits between 2 dates
-	repo forall -pc 'git log --pretty=format:"[%h]  %s" --decorate --after=$After_Date --until=$Until_Date' >> $Changelog
+	echo '====================' >> $Changelog;
+	echo  "     "$Until_Date       >> $Changelog;
+	echo '===================='	>> $Changelog;
 	echo >> $Changelog;
-        echo >> $Changelog;
+
+	# Cycle through every repo to find commits between 2 dates
+	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
+	echo >> $Changelog;
 done
 
 sed -i 's/project/   */g' $Changelog
